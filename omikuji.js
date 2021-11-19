59d41debaa335f8f549b7478bbb39e9a1d688d3a
津嶋勇輝 <67349483+tsushima3074@users.noreply.github.com>
2021-11-19 10:19:10 +0900

    [Update]使用する変数の宣言

--- omikuji.js
+++ omikuji.js
@@ -1,3 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
-
+    // テキストではletを使っているが、usernameとuserresultは読み込み時に代入するのでconstの方が望ましい
+    // jsはキャメルケースでの記述が推奨されているので変数名を少し改良
+    const userName;
+    const userResult;
 }, false);
