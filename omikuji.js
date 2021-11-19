09cdc27372ffe5ca55f3f070fa9b073da707796d
津嶋勇輝 <67349483+tsushima3074@users.noreply.github.com>
2021-11-19 10:31:22 +0900

    [Add]入力された名前の表示

--- omikuji.js
+++ omikuji.js
@@ -3,4 +3,6 @@ document.addEventListener("DOMContentLoaded", () => {
     // constでは資料のような定義ができないので処理を変更
     // jsはキャメルケースでの記述が推奨されているので変数名を少し改良
     const userName = prompt("お名前を教えてください。");
+    // 入力された名前の表示　未入力の場合は`名無し`と表示
+    document.getElementById("name").innerHTML = userName !== "" ? userName : "名無し"; 
 }, false);
