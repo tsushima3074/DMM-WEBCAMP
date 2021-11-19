41c04a4ff12f897b5f261a0a2b583c70771ea05b
津嶋勇輝 <67349483+tsushima3074@users.noreply.github.com>
2021-11-19 10:40:27 +0900

    [Update]結果の表示昨日の完成

--- omikuji.js
+++ omikuji.js
@@ -5,4 +5,14 @@ document.addEventListener("DOMContentLoaded", () => {
     const userName = prompt("お名前を教えてください。");
     // 入力された名前の表示　未入力の場合は`名無し`と表示
     document.getElementById("name").innerHTML = userName !== "" ? userName : "名無し"; 
+
+    // 乱数の生成
+    // const rand = Math.floor(Math.random() * 5);
+
+    // テキストでは`==`を使っているが、jsでは型の比較までをしないので厳密に書くのであれば`===`を使った方がよい
+    // 0~4を生成するのであれば、配列を作成しておき、添え字として使った方が簡単なので処理を変更
+    const userResult = ["大吉", "中吉", "小吉", "吉", "凶"];
+    // さらにこの方法を使えば`rand`という変数を使う必要がなくなるのでメモリを食わなくなる方法に変更できる
+    // 結果の表示
+    document.getElementById("result").innerHTML = userResult[Math.floor(Math.random() * 5)];
 }, false);
