c2bb3c79a38ca253b16593fbee663de069b8effc
津嶋勇輝 <67349483+tsushima3074@users.noreply.github.com>
2021-11-19 10:28:25 +0900

    [Fix]変数の宣言の仕方を変更

--- omikuji.js
+++ omikuji.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
     // テキストではletを使っているが、usernameとuserresultは読み込み時に代入するのでconstの方が望ましい
+    // constでは資料のような定義ができないので処理を変更
     // jsはキャメルケースでの記述が推奨されているので変数名を少し改良
-    const userName;
-    const userResult;
+    const userName = prompt("お名前を教えてください。");
 }, false);
